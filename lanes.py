294f4e5aaa0356e5da139300053208db2b9782d4
Vivek Sainanee <viveksainanee@gmail.com>
2019-03-09 10:23:00 -0800

    refactored to use matplotlib

--- lanes.py
+++ lanes.py
@@ -1,30 +1,53 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
+#canny function takes in an image
+def canny(image):
+    #convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+    #verify it turned gray
+    # print('gray')
+    # print(gray)
+
+    blur = cv2.GaussianBlur(gray, (5,5),0)
+
+    #verify it applies Gaussian Blur
+    # print('blur')
+    # print(blur)
+
+
+    #canny looks at the gradient by calculating the
+    #derivative between cells
+    canny = cv2.Canny(blur,50,150)
+
+    #verify it uses canny
+    # print('canny')
+    # print(canny)
+
+    return canny
+
+
+
 
 #imports the image
 image = cv2.imread('test_image.jpg')
 
-#verify it is a 2D array
-print(image)
+#verify we can see the image (3d array)
+# print('base image')
+# print(image)
 
 #copy the array into a new var
 lane_image = np.copy(image)
 
-#convert to grayscale
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+canny = canny(lane_image)
 
-#verify it turned gray
-print(gray)
 
-blur = cv2.GaussianBlur(gray, (5,5),0)
-
-#canny looks at the gradient by calculating the
-#derivative between cells
-canny = cv2.Canny(blur,50,150)
 
 
 #show the image
-cv2.imshow('result', canny)
+plt.imshow(canny)
 
 #show the image indefinitely until keypress
-cv2.waitKey(0)
+plt.show()
